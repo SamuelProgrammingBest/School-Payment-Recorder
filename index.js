969153175339fbc4882fba27abc757ecba329c0b
SamuelProgrammingBest <adewalesamuel835@gmail.com>
2026-03-27 10:23:56 +0100

    Wassup

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const rateLimit = require("express-rate-limit");
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "http://localhost:5173",
   credentials: true
 }))
 
